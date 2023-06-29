65628cc89ee2cbcd58f1efe6a6c576035116186e
xtayex <liyuliang2022_sei@stu.ecnu.edu.cn>
2023-06-29 13:08:45 +0000

    add many operators

--- examples/MLIRPython/broadcast_add.py
+++ examples/MLIRPython/broadcast_add.py
@@ -6,6 +6,6 @@ def foo(x, y):
   return x + y
 
 foo_mlir = dynamo.optimize(DynamoCompiler)(foo)
-in1 = torch.randn(10)
-in2 = torch.randn(1)
+in1 = torch.randn((10, 2))
+in2 = torch.randn((1, 2))
 foo_mlir(in1, in2)
--- examples/MLIRPython/buddy/operators_gen.py
+++ examples/MLIRPython/buddy/operators_gen.py
@@ -1,9 +1,12 @@
 import array
 
 from mlir.ir import (RankedTensorType,
-                     F32Type, 
-                     DenseElementsAttr, 
-                     FloatAttr)
+                     F32Type,
+                     IntegerType,
+                     DenseElementsAttr,
+                     DenseIntElementsAttr,
+                     FloatAttr,
+                     IntegerAttr)
 from mlir.dialects import (arith,
                            linalg,
                            tosa,
@@ -24,10 +27,8 @@ def _broadcast_shape(tensor_input1, tensor_input2):
   shp2 = RankedTensorType(tensor_input2.type).shape
   shp1_list = list(shp1)
   shp2_list = list(shp2)
-  swapped = False
   if len(shp1_list) < len(shp2_list):
     shp1_list, shp2_list = shp2_list, shp1_list
-    swapped = True
   while len(shp2_list) < len(shp1_list):
     shp2_list.insert(0, 1)
   for idx, (dim1, dim2) in enumerate(zip(shp1_list, shp2_list)):
@@ -47,56 +48,14 @@ def AddOp(node, symbolTable):
   return op
 
 
-def MatmulOp(node, symbolTable):
-  # Get two input values.
+def GtOp(node, symbolTable):
   input1 = symbolTable.get(str(node._args[0]))
   input2 = symbolTable.get(str(node._args[1]))
-  shp1 = RankedTensorType(input1.type).shape
-  shp2 = RankedTensorType(input2.type).shape
-  assert len(shp1) == len(shp2)
+  broadcasted_shp = _broadcast_shape(input1, input2)
+  sizes = broadcasted_shp
   f32 = F32Type.get()
-  zeroElement = FloatAttr.get(f32, 0.0)
-  if len(shp1) == 2:
-    # Infer the output sizes.
-    size1 = shp1[0]
-    size2 = shp2[1]
-    sizes = [size1, size2]
-    # Generate an output tensor for matmul operation.
-    # For example:
-    # `arith.constant dense<0.000000e+00> : tensor<3x3xf32>`
-    tensorType = RankedTensorType.get(sizes, f32)
-    attr = DenseElementsAttr.get_splat(tensorType, zeroElement)
-    initResult = arith.ConstantOp(tensorType, attr).result
-    # Generate matmul operation.
-    op = linalg.matmul(input1, input2, outs=[initResult])
-    return op
-  elif len(shp1) == 3:
-    size0 = shp1[0]
-    size1 = shp1[1]
-    size2 = shp2[2]
-    sizes = [size0, size1, size2]
-    tensorType = RankedTensorType.get(sizes, f32)
-    attr = DenseElementsAttr.get_splat(tensorType, zeroElement)
-    initResult = arith.ConstantOp(tensorType, attr)
-    op = linalg.batch_matmul(input1, input2, outs=[initResult.result])
-    return op
-  else:
-    raise NotImplementedError
-
-
-def TransposeOp(node, symbolTable):
-  input_tensor = symbolTable.get(str(node._args[0]))
-  size1 = RankedTensorType(input_tensor.type).shape[0]
-  size2 = RankedTensorType(input_tensor.type).shape[1]
-  sizes = [size2, size1]
-
-  f32 = F32Type.get()
-  transResultTensorType = RankedTensorType.get(sizes, f32)
-  permTensorType = RankedTensorType.get([2], f32)
-  permContent = memoryview(array.array('i', [1, 0]))
-  permAttr = DenseElementsAttr.get(permContent)
-  perm = arith.ConstantOp(permTensorType, permAttr)
-  op = tosa.TransposeOp(transResultTensorType, input_tensor, perm)
+  addResultTensorType = RankedTensorType.get(sizes, f32)
+  op = tosa.GreaterOp(addResultTensorType, input1, input2)
   return op
 
 
@@ -110,34 +69,23 @@ def SubOp(node, symbolTable):
 def MulOp(node, symbolTable):
   input1 = symbolTable.get(str(node._args[0])) 
   input2 = symbolTable.get(str(node._args[1]))
-  op = arith.MulFOp(input1, input2)
+  broadcasted_shp = _broadcast_shape(input1, input2)
+  sizes = broadcasted_shp
+  f32 = F32Type.get()
+  addResultTensorType = RankedTensorType.get(sizes, f32)
+  op = tosa.MulOp(addResultTensorType, input1, input2)
   return op
 
 
-def TrueDivOp(node, symbolTable):
+def DivOp(node, symbolTable):
   input1 = symbolTable.get(str(node._args[0]))
   input2 = symbolTable.get(str(node._args[1]))
-  op = arith.DivFOp(input1, input2)
+  broadcasted_shp = _broadcast_shape(input1, input2)
+  sizes = broadcasted_shp
+  f32 = F32Type.get()
+  divResultTensorType = RankedTensorType.get(sizes, f32)
+  op = tosa.DivOp(divResultTensorType, input1, input2)
   return op
-
-
-def OnesOp(node, symbolTable):
-  args = node._args
-  # flatten the size tuple
-  while isinstance(args[0], tuple):
-    args = args[0]
-  sizes = list(args)
-  op = _getConstantTensorOp(1.0, sizes)
-  return op
-
-
-def AddmmOp(node, symbolTable):
-  input_ = symbolTable.get(str(node._args[0]))
-  mat1 = symbolTable.get(str(node._args[1]))
-  mat2 = symbolTable.get(str(node._args[2]))
-  mmOp = MatmulOp(node, symbolTable)
-  addOp = arith.AddFOp(input_, mmOp.result)
-  return addOp
 
 
 def ErfOp(node, symbolTable):
@@ -147,21 +95,49 @@ def ErfOp(node, symbolTable):
 
 
 def TanhOp(node, symbolTable):
-  input_ = symbolTable.get(str(node._args[0]))
-  tanhOp = math.TanhOp(input_)
+  input1 = symbolTable.get(str(node._args[0]))
+  sizes = RankedTensorType(input1.type).shape
+  f32 = F32Type.get()
+  tanhResultTensorType = RankedTensorType.get(sizes, f32)
+  tanhOp = tosa.TanhOp(tanhResultTensorType, input1)
   return tanhOp
 
 
 def ExpOp(node, symbolTable):
-  input_ = symbolTable.get(str(node._args[0]))
-  expOp = math.ExpOp(input_)
+  input1 = symbolTable.get(str(node._args[0]))
+  sizes = RankedTensorType(input1.type).shape
+  f32 = F32Type.get()
+  expResultTensorType = RankedTensorType.get(sizes, f32)
+  expOp = tosa.ExpOp(expResultTensorType, input1)
   return expOp
 
 
 def RsqrtOp(node, symbolTable):
-  input_ = symbolTable.get(str(node._args[0]))
-  rsqrtOp = math.RsqrtOp(input_)
+  input1 = symbolTable.get(str(node._args[0]))
+  sizes = RankedTensorType(input1.type).shape
+  f32 = F32Type.get()
+  rsqrtResultTensorType = RankedTensorType.get(sizes, f32)
+  rsqrtOp = tosa.RsqrtOp(rsqrtResultTensorType, input1)
   return rsqrtOp
+
+
+def AmaxOp(node, symbolTable):
+  input1 = symbolTable.get(str(node._args[0]))
+  dim = symbolTable.get(str(node._args[1]))[0]
+  signlessType = IntegerType.get_signless()
+  dimAttr = IntegerAttr.get(signlessType, dim)
+  op = tosa.ReduceMaxOp(input1, dimAttr)
+  return op
+
+
+def ReshapeOp(node, symbolTable):
+  input1 = symbolTable.get(str(node._args[0]))
+  newShape = symbolTable.get(str(node._args[1]))[0]
+  newShapeContent = array.array('Q', newShape)
+  newShapeContent = memoryview(newShapeContent)
+  newShapeAttr = DenseElementsAttr.get(newShapeContent)
+  op = tosa.ReshapeOp(input1, newShapeContent)
+  return op
 
 
 # add, addmm, amax, bmm, clone, convert_element_type
@@ -169,12 +145,4 @@ def RsqrtOp(node, symbolTable):
 # inductor_random, inductor_seeds, mul, permute, reshape, rsqrt
 # select, slice, sub, tanh, unsqueeze, var_mean
 OpCodeGen = {
-  'add': AddOp,
-  'iadd': AddOp,
-  'matmul': MatmulOp,
-  'transpose': TransposeOp,
-  'sub': SubOp,
-  'mul': MulOp,
-  'truediv': TrueDivOp,
-  'ones': OnesOp,
 }
