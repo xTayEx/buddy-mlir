f25a45531e14bc80df1f06224347ca0816fe15bb
xtayex <liyuliang2022_sei@stu.ecnu.edu.cn>
2023-07-04 15:12:38 +0000

    refine compiler.py. make it simpler to implement multiple results

--- examples/MLIRPython/buddy/compiler.py
+++ examples/MLIRPython/buddy/compiler.py
@@ -1,106 +1,111 @@
+import operator
 from typing import List
 
 import mlir.dialects.func as func
+from mlir.dialects import arith
 import torch
 
-import mlir.ir
-from mlir.ir import *
+import mlir.ir as ir
 from mlir.passmanager import *
-from loguru import logger
 
-from .operators_gen import OpCodeGen
+from .operators_gen import operation_func
 
 
 def DynamoCompiler(gm: torch.fx.GraphModule, inputs: List[torch.Tensor]):
-  print("Custom Compiler from FX Graph to MLIR:")
-  print("-------------------------------------------------------------------")
-  gm.graph.print_tabular()
-  # Initialize the MLIR context.
-  ctx = Context()
-  with Location.unknown(ctx):
-    module = Importer(gm, inputs)
-    module = Lowering(module)
-  return gm.forward
+    print("Custom Compiler from FX Graph to MLIR:")
+    print("-------------------------------------------------------------------")
+    gm.graph.print_tabular()
+    # Initialize the MLIR context.
+    ctx = ir.Context()
+    with ir.Location.unknown(ctx):
+        fx_importer = _FXGraphImporter(gm, inputs)
+        module = fx_importer.import_graph()
+        module = Lowering(module)
+    return gm.forward
 
 
-def Importer(gm: torch.fx.GraphModule, inputs: List[torch.Tensor]):
-  # Initialize the symbol table.
-  symbolTable = {}
-  # Create a module and build the operations into the module.
-  module = Module.create()
-  with InsertionPoint(module.body):
-    # Parse the arguments.
-    arguments = []
-    for arg in inputs:
-      shapeList = list(arg.shape)
-      f32 = F32Type.get()
-      tensorArg = RankedTensorType.get(shapeList, f32)
-      arguments.append(tensorArg)
+class _FXGraphImporter:
+    def __init__(self, gm: torch.fx.GraphModule, inputs: List[torch.Tensor], func_name: str = "main"):
+        self._symbol_table = {}
+        self._gm = gm
+        self._func_name = func_name
+        self._inputs = inputs
+        self._num_input_visited = 0
+        self._module = ir.Module.create()
 
-    # Generate the function.
-    @func.FuncOp.from_py_func(*arguments)
-    def generated_func(*args):
-      # Convert arguments tuple into a list.
-      argsList = list(args)
-      # Traverse the graph and generate IR.
-      for node in gm.graph.nodes:
-        if node.op != "output":
-          symbolTable[str(node.name)] = CodeGen(node, symbolTable, argsList)
-        else:
-          symbolTable["output"] = CodeGen(node, symbolTable, argsList)
+    def import_graph(self):
+        with ir.InsertionPoint(self._module.body):
+            arguments = []
+            for arg in self._inputs:
+                shape_list = list(arg.shape)
+                f32 = ir.F32Type.get()
+                tensor_arg = ir.RankedTensorType.get(shape_list, f32)
+                arguments.append(tensor_arg)
 
-      return symbolTable.get("output")
-  print("-------------------------------------------------------------------")
-  print("Printing the symbol table ...")
-  for symbol, op in symbolTable.items():
-    print(symbol, ": ", op)
-  print("-------------------------------------------------------------------")
-  print("Printing the generated MLIR ...")
-  print(module)
-  return module
+            @func.FuncOp.from_py_func(*arguments, name=self._func_name)
+            def generated_func(*args):
+                args_list = list(args)
+                for node in self._gm.graph.nodes:
+                    if node.op == "output":
+                        output_node_args = node.args[0]
+                        returns = []
+                        for output_arg in output_node_args:
+                            op = self._symbol_table.get((str(output_arg), 0))
+                            returns.append(op)
+
+                        self._symbol_table[("output", 0)] = returns
+                    elif node.op == "placeholder":
+                        self._import_placeholder(node, args_list)
+                    else:
+                        if node.target is operator.getitem:
+                            self._symbol_table[(str(node.name), 0)] = self._symbol_table[(node.args[0], node.args[1])]
+                        else:
+                            self._import_op(node)
+
+                return self._symbol_table.get(("output", 0))
+
+        print("Printing the generated MLIR")
+        print(self._module)
+        return self._module
+
+    def _import_placeholder(self, node: torch.fx.Node, args_list):
+        placeholder_name = args_list[self._num_input_visited]
+        self._symbol_table[(str(node.name), 0)] = placeholder_name
+        self._num_input_visited += 1
+
+    def _import_op(self, node: torch.fx.Node):
+        op_code = node.target.__name__
+
+        operation: ir.Operation = operation_func[op_code](node, self._symbol_table)
+        for i, result in enumerate(operation.results):
+            self._symbol_table[(str(node.name), i)] = result
 
 
-def CodeGen(node, symbolTable, argsList):
-  if node.op == "placeholder":
-    # Bind the placeholder with args.
-    placeholderName = argsList[0]
-    argsList.pop(0)
-    return placeholderName
-  if node.op == "call_function":
-    # Parse a call_function operation.
-    opName = node.target.__name__
-    return OpCodeGen[opName](node, symbolTable)
-  if node.op == "output":
-    # Generating return operation.
-    ret = symbolTable.get(str(node._args[0][0]))
-    return ret
-
-
-def Lowering(module: Module):
-  print("-------------------------------------------------------------------")
-  print("Bufferizing the module ...")
-  pm = PassManager('builtin.module')
-  pm.add("func.func(tosa-to-linalg)")
-  pm.add("func.func(tosa-to-tensor)")
-  pm.add("empty-tensor-to-alloc-tensor")
-  pm.add("convert-elementwise-to-linalg")
-  pm.add("arith-bufferize")
-  pm.add("func.func(linalg-bufferize)")
-  pm.add("func.func(tensor-bufferize)")
-  pm.add("func-bufferize")
-  pm.run(module.operation)
-  print(module)
-  print("-------------------------------------------------------------------")
-  print("Lowering the module to LLVM dialect ...")
-  pm.add("func.func(buffer-deallocation)")
-  pm.add("func.func(convert-linalg-to-loops)")
-  pm.add("convert-scf-to-cf")
-  pm.add("convert-linalg-to-llvm")
-  pm.add("convert-arith-to-llvm")
-  pm.add("expand-strided-metadata")
-  pm.add("finalize-memref-to-llvm")
-  pm.add("convert-func-to-llvm")
-  pm.add("reconcile-unrealized-casts")
-  pm.run(module.operation)
-  print(module)
-  return module
+def Lowering(module: ir.Module):
+    print("-------------------------------------------------------------------")
+    print("Bufferizing the module ...")
+    pm = PassManager("builtin.module")
+    pm.add("func.func(tosa-to-linalg)")
+    pm.add("func.func(tosa-to-tensor)")
+    pm.add("empty-tensor-to-alloc-tensor")
+    pm.add("convert-elementwise-to-linalg")
+    pm.add("arith-bufferize")
+    pm.add("func.func(linalg-bufferize)")
+    pm.add("func.func(tensor-bufferize)")
+    pm.add("func-bufferize")
+    pm.run(module.operation)
+    print(module)
+    print("-------------------------------------------------------------------")
+    print("Lowering the module to LLVM dialect ...")
+    pm.add("func.func(buffer-deallocation)")
+    pm.add("func.func(convert-linalg-to-loops)")
+    pm.add("convert-scf-to-cf")
+    pm.add("convert-linalg-to-llvm")
+    pm.add("convert-arith-to-llvm")
+    pm.add("expand-strided-metadata")
+    pm.add("finalize-memref-to-llvm")
+    pm.add("convert-func-to-llvm")
+    pm.add("reconcile-unrealized-casts")
+    pm.run(module.operation)
+    print(module)
+    return module
--- examples/MLIRPython/buddy/operators_gen.py
+++ examples/MLIRPython/buddy/operators_gen.py
@@ -1,190 +1,190 @@
 import array
 
-from mlir.ir import (RankedTensorType,
-                     F32Type,
-                     IntegerType,
-                     FloatAttr,
-                     DenseI64ArrayAttr,
-                     DenseElementsAttr,
-                     IntegerAttr)
-from mlir.dialects import (arith,
-                           tosa,
-                           math)
+from mlir.ir import (
+    RankedTensorType,
+    F32Type,
+    Module,
+    IntegerType,
+    FloatAttr,
+    DenseI64ArrayAttr,
+    DenseElementsAttr,
+    IntegerAttr,
+)
+from mlir.dialects import arith, tosa, math
+import mlir.dialects.func as func
 
 
 def _getConstantTensorOp(value: float, sizes: list[int]):
-  f32 = F32Type.get()
-  constantTensorType = RankedTensorType.get(sizes, f32)
-  constantElementAttr = FloatAttr.get(f32, value)
-  constantTensorAttr = DenseElementsAttr.get_splat(constantTensorType, constantElementAttr)
-  op = arith.ConstantOp(constantTensorType, constantTensorAttr)
-  return op
+    f32 = F32Type.get()
+    constantTensorType = RankedTensorType.get(sizes, f32)
+    constantElementAttr = FloatAttr.get(f32, value)
+    constantTensorAttr = DenseElementsAttr.get_splat(
+        constantTensorType, constantElementAttr
+    )
+    op = arith.ConstantOp(constantTensorType, constantTensorAttr)
+    return op
 
 
 def _broadcast_shape(tensor_input1, tensor_input2):
-  shp1 = RankedTensorType(tensor_input1.type).shape
-  shp2 = RankedTensorType(tensor_input2.type).shape
-  if len(shp1) < len(shp2):
-    shp1, shp2 = shp2, shp1
-  while len(shp2) < len(shp1):
-    shp2.insert(0, 1)
-  for idx, (dim1, dim2) in enumerate(zip(shp1, shp2)):
-    shp1[idx] = shp2[idx] = max(dim1, dim2)
+    shp1 = RankedTensorType(tensor_input1.type).shape
+    shp2 = RankedTensorType(tensor_input2.type).shape
+    if len(shp1) < len(shp2):
+        shp1, shp2 = shp2, shp1
+    while len(shp2) < len(shp1):
+        shp2.insert(0, 1)
+    for idx, (dim1, dim2) in enumerate(zip(shp1, shp2)):
+        shp1[idx] = shp2[idx] = max(dim1, dim2)
 
-  return shp1
-
-
-def AddOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  input2 = symbolTable.get(str(node.args[1]))
-  broadcasted_shp = _broadcast_shape(input1, input2)
-  sizes = broadcasted_shp
-  f32 = F32Type.get()   
-  addResultTensorType = RankedTensorType.get(sizes, f32)
-  op = tosa.AddOp(addResultTensorType, input1, input2)
-  return op
+    return shp1
 
 
-def GtOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  input2 = symbolTable.get(str(node.args[1]))
-  broadcasted_shp = _broadcast_shape(input1, input2)
-  sizes = broadcasted_shp
-  f32 = F32Type.get()
-  addResultTensorType = RankedTensorType.get(sizes, f32)
-  op = tosa.GreaterOp(addResultTensorType, input1, input2)
-  return op
+def AddOp(node, symbol_table):
+    input1 = symbol_table.get((str(node.args[0]), 0))
+    input2 = symbol_table.get((str(node.args[1]), 0))
+    broadcasted_shp = _broadcast_shape(input1, input2)
+    sizes = broadcasted_shp
+    f32 = F32Type.get()
+    addResultTensorType = RankedTensorType.get(sizes, f32)
+    op = tosa.AddOp(addResultTensorType, input1, input2)
+    return op
 
 
-def SubOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  input2 = symbolTable.get(str(node.args[1]))
-  op = arith.SubFOp(input1, input2)
-  return op
+def GtOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    input2 = symbol_table.get(str(node.args[1]))
+    broadcasted_shp = _broadcast_shape(input1, input2)
+    sizes = broadcasted_shp
+    f32 = F32Type.get()
+    addResultTensorType = RankedTensorType.get(sizes, f32)
+    op = tosa.GreaterOp(addResultTensorType, input1, input2)
+    return op
 
 
-def MulOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  input2 = symbolTable.get(str(node.args[1]))
-  broadcasted_shp = _broadcast_shape(input1, input2)
-  sizes = broadcasted_shp
-  f32 = F32Type.get()
-  addResultTensorType = RankedTensorType.get(sizes, f32)
-  op = tosa.MulOp(addResultTensorType, input1, input2)
-  return op
+def SubOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    input2 = symbol_table.get(str(node.args[1]))
+    op = arith.SubFOp(input1, input2)
+    return op
 
 
-def DivOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  input2 = symbolTable.get(str(node.args[1]))
-  broadcasted_shp = _broadcast_shape(input1, input2)
-  sizes = broadcasted_shp
-  f32 = F32Type.get()
-  divResultTensorType = RankedTensorType.get(sizes, f32)
-  op = tosa.DivOp(divResultTensorType, input1, input2)
-  return op
+def MulOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    input2 = symbol_table.get(str(node.args[1]))
+    broadcasted_shp = _broadcast_shape(input1, input2)
+    sizes = broadcasted_shp
+    f32 = F32Type.get()
+    addResultTensorType = RankedTensorType.get(sizes, f32)
+    op = tosa.MulOp(addResultTensorType, input1, input2)
+    return op
 
 
-def ErfOp(node, symbolTable):
-  input_ = symbolTable.get(str(node.args[0]))
-  erfOp = math.ErfOp(input_)
-  return erfOp
+def DivOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    input2 = symbol_table.get(str(node.args[1]))
+    broadcasted_shp = _broadcast_shape(input1, input2)
+    sizes = broadcasted_shp
+    f32 = F32Type.get()
+    divResultTensorType = RankedTensorType.get(sizes, f32)
+    op = tosa.DivOp(divResultTensorType, input1, input2)
+    return op
 
 
-def TanhOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  sizes = RankedTensorType(input1.type).shape
-  f32 = F32Type.get()
-  tanhResultTensorType = RankedTensorType.get(sizes, f32)
-  tanhOp = tosa.TanhOp(tanhResultTensorType, input1)
-  return tanhOp
+def ErfOp(node, symbol_table):
+    input_ = symbol_table.get(str(node.args[0]))
+    erfOp = math.ErfOp(input_)
+    return erfOp
 
 
-def ExpOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  sizes = RankedTensorType(input1.type).shape
-  f32 = F32Type.get()
-  expResultTensorType = RankedTensorType.get(sizes, f32)
-  expOp = tosa.ExpOp(expResultTensorType, input1)
-  return expOp
+def TanhOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    sizes = RankedTensorType(input1.type).shape
+    f32 = F32Type.get()
+    tanhResultTensorType = RankedTensorType.get(sizes, f32)
+    tanhOp = tosa.TanhOp(tanhResultTensorType, input1)
+    return tanhOp
 
 
-def RsqrtOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  sizes = RankedTensorType(input1.type).shape
-  f32 = F32Type.get()
-  rsqrtResultTensorType = RankedTensorType.get(sizes, f32)
-  rsqrtOp = tosa.RsqrtOp(rsqrtResultTensorType, input1)
-  return rsqrtOp
+def ExpOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    sizes = RankedTensorType(input1.type).shape
+    f32 = F32Type.get()
+    expResultTensorType = RankedTensorType.get(sizes, f32)
+    expOp = tosa.ExpOp(expResultTensorType, input1)
+    return expOp
 
 
-def AmaxOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  dim = symbolTable.get(str(node.args[1]))[0]
-  signlessType = IntegerType.get_signless()
-  dimAttr = IntegerAttr.get(signlessType, dim)
-  op = tosa.ReduceMaxOp(input1, dimAttr)
-  return op
+def RsqrtOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    sizes = RankedTensorType(input1.type).shape
+    f32 = F32Type.get()
+    rsqrtResultTensorType = RankedTensorType.get(sizes, f32)
+    rsqrtOp = tosa.RsqrtOp(rsqrtResultTensorType, input1)
+    return rsqrtOp
 
 
-def ReshapeOp(node, symbolTable):
-  input1 = symbolTable.get(str(node.args[0]))
-  newShape = symbolTable.get(str(node.args[1]))
-  newShapeContent = array.array('i', newShape)
-  newShapeContent = memoryview(newShapeContent)
-  newShapeAttr = DenseElementsAttr.get(newShapeContent)
-  op = tosa.ReshapeOp(input1, newShapeContent)
-  return op
+def AmaxOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    dim = symbol_table.get(str(node.args[1]))[0]
+    signlessType = IntegerType.get_signless()
+    dimAttr = IntegerAttr.get(signlessType, dim)
+    op = tosa.ReduceMaxOp(input1, dimAttr)
+    return op
 
 
-def UnsqueezeOp(node, symbolTable):
-  inputTensor = symbolTable.get(str(node.args[0]))
-  dim = symbolTable.get(str(node.args[1]))
-  sizes = RankedTensorType(inputTensor.type).shape
-  sizes.insert(dim, 1)
-  newShapeContent = array.array('i', sizes)
-  newShapeContent = memoryview(newShapeContent)
-  newShapeAttr = DenseElementsAttr.get(newShapeContent)
-  op = tosa.ReshapeOp(inputTensor, newShapeAttr)
-  return op
+def ReshapeOp(node, symbol_table):
+    input1 = symbol_table.get(str(node.args[0]))
+    newShape = symbol_table.get(str(node.args[1]))
+    newShapeContent = array.array("i", newShape)
+    newShapeContent = memoryview(newShapeContent)
+    newShapeAttr = DenseElementsAttr.get(newShapeContent)
+    op = tosa.ReshapeOp(input1, newShapeContent)
+    return op
 
 
-def GetItemOp(node, symbolTable):
-  inputTensor = symbolTable.get(str(node.args[0]))
-  index = node.args[1]
-  sizes = RankedTensorType(inputTensor.type).shape
+def UnsqueezeOp(node, symbol_table):
+    inputTensor = symbol_table.get(str(node.args[0]))
+    dim = symbol_table.get(str(node.args[1]))
+    sizes = RankedTensorType(inputTensor.type).shape
+    sizes.insert(dim, 1)
+    newShapeContent = array.array("i", sizes)
+    newShapeContent = memoryview(newShapeContent)
+    newShapeAttr = DenseElementsAttr.get(newShapeContent)
+    op = tosa.ReshapeOp(inputTensor, newShapeAttr)
+    return op
 
-  print(f"len(sizes): {len(sizes)}")
-  startContent = array.array('Q', [index, 0])
-  startContent = memoryview(startContent)
-  startAttr = DenseI64ArrayAttr.get(startContent)
 
-  stridesContent = array.array('Q', [1, sizes[1]])
-  stridesContent = memoryview(stridesContent)
-  strideAttr = DenseI64ArrayAttr.get(stridesContent)
+def GetItemOp(node, symbol_table):
+    inputTensor = symbol_table.get(str(node.args[0]))
+    index = node.args[1]
+    sizes = RankedTensorType(inputTensor.type).shape
 
-  f32 = F32Type.get()
-  resultSizes = [1, *sizes[1:]]
-  resultType = RankedTensorType.get(resultSizes, f32)
+    startContent = array.array("Q", [index, 0])
+    startContent = memoryview(startContent)
+    startAttr = DenseI64ArrayAttr.get(startContent)
 
-  op = tosa.SliceOp(resultType, inputTensor, startAttr, strideAttr)
-  return op
+    stridesContent = array.array("Q", [1, sizes[1]])
+    stridesContent = memoryview(stridesContent)
+    strideAttr = DenseI64ArrayAttr.get(stridesContent)
+
+    f32 = F32Type.get()
+    resultSizes = [1, *sizes[1:]]
+    resultType = RankedTensorType.get(resultSizes, f32)
+
+    op = tosa.SliceOp(resultType, inputTensor, startAttr, strideAttr)
+    return op
 
 
 # 从文档以及print_tabular的结果来看，dim参数可能是None，此时需要对所有维度一同计算var和mean；
 # 也可能是一个包裹在list中的int，指定具体的某个维度；也可能是包裹在list中的tuple，指定具体的某几个维度
-def VarMeanOp(node, symbolTable):
-  inputTensor = symbolTable.get(str(node.args[0]))
-  dim = node.args[1]
-  if isinstance(dim, list):
-    dim = dim[0]
+def VarMeanOp(node, symbol_table):
+    inputTensor = symbol_table.get(str(node.args[0]))
+    dim = node.args[1]
+    if isinstance(dim, list):
+        dim = dim[0]
 
 
 # add, addmm, amax, bmm, clone, convert_element_type
 # div, embedding, erf, exp, expand, getitem, gt, inductor_lookup_seed
 # inductor_random, inductor_seeds, mul, permute, reshape, rsqrt
 # select, slice, sub, tanh, unsqueeze, var_mean
-OpCodeGen = {
-  'getitem': GetItemOp,
-  'add': AddOp
-}
+operation_func = {"getitem": GetItemOp, "add": AddOp}
