ae3302f7632c9538d1c84d85751658aeba3e5004
xtayex <liyuliang2022_sei@stu.ecnu.edu.cn>
2024-02-04 22:39:18 +0800

    force sync with main

--- examples/BuddyLlama/import-llama2.py
+++ examples/BuddyLlama/import-llama2.py
@@ -19,17 +19,73 @@
 # ===---------------------------------------------------------------------------
 
 import os
-import time
+from pathlib import Path
+from typing import Optional, List
 
-import numpy
 import torch
-from transformers import LlamaForCausalLM, LlamaTokenizer
-from torch._functorch.aot_autograd import aot_autograd_decompositions
 from torch._inductor.decomposition import decompositions as inductor_decomp
+from torch._functorch.aot_autograd import aot_module_simplified
 
 from buddy.compiler.frontend import DynamoCompiler
 from buddy.compiler.ops import tosa
+from transformer_with_kvcache import Transformer
 
+
+def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
+    def _compiler(
+        _gm: torch.fx.GraphModule, _example_inputs: List[torch.Tensor]
+    ):
+        _gm.graph.print_tabular()
+        return _gm.forward
+
+    return aot_module_simplified(
+        gm,
+        example_inputs,
+        fw_compiler=_compiler,
+        decompositions=inductor_decomp,
+    )  # return a python callable
+
+
+def multinomial_sample_one_no_sync(
+    probs_sort,
+):  # Does multinomial sampling without a cuda synchronization
+    q = torch.empty_like(probs_sort).exponential_(1)
+    return torch.argmax(probs_sort / q, dim=-1, keepdim=True).to(
+        dtype=torch.int
+    )
+
+
+def logits_to_probs(
+    logits, temperature: float = 1.0, top_k: Optional[int] = None
+):
+    logits = logits / max(temperature, 1e-5)
+
+    if top_k is not None:
+        v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+        pivot = v.select(-1, -1).unsqueeze(-1)
+        logits = torch.where(logits < pivot, -float("Inf"), logits)
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    return probs
+
+
+def sample(logits, temperature: float = 1.0, top_k: Optional[int] = None):
+    probs = logits_to_probs(logits[0, -1], temperature, top_k)
+    idx_next = multinomial_sample_one_no_sync(probs)
+    return idx_next, probs
+
+
+def prefill(
+    model_: Transformer,
+    x: torch.Tensor,
+    input_pos_: torch.Tensor,
+    **sampling_kwargs
+) -> torch.Tensor:
+    # input_pos: [B, S]
+    logits = model_(x, input_pos_)
+    return sample(logits, **sampling_kwargs)[0]
+
+
+MAX_NEW_TOKENS = 40
 
 # Retrieve the LLaMA model path from environment variables.
 model_path = os.environ.get("LLAMA_MODEL_PATH")
@@ -38,10 +94,14 @@ if model_path is None:
         "The environment variable 'LLAMA_MODEL_PATH' is not set or is invalid."
     )
 
-# Initialize the tokenizer and model from the specified model path.
-tokenizer = LlamaTokenizer.from_pretrained(model_path)
-model = LlamaForCausalLM.from_pretrained(model_path, torchscript=True)
-model.config.use_cache = False
+model = Transformer.from_name(model_path)
+checkpoint = torch.load(
+    str(Path(model_path) / "model.pth"), mmap=True, weights_only=True
+)
+model.load_state_dict(checkpoint, assign=True)
+model = model.to(device="cpu", dtype=torch.float32)
+model.eval()
+
 
 # Initialize Dynamo Compiler with specific configurations as an importer.
 dynamo_compiler = DynamoCompiler(
@@ -49,22 +109,50 @@ dynamo_compiler = DynamoCompiler(
     aot_autograd_decomposition=inductor_decomp,
 )
 
-# Import the model into MLIR module and parameters.
+prompt = torch.tensor([1 for _ in range(10)], dtype=torch.int64)
+T = prompt.size(0)
+T_new = T + MAX_NEW_TOKENS
+max_seq_length = min(T_new, model.config.block_size)
+
+model.setup_caches(1, max_seq_length)
+empty = torch.empty(T_new, dtype=torch.int64)
+empty[:T] = prompt
+seq = empty
+input_pos = torch.arange(0, T, dtype=torch.int64)
+
 with torch.no_grad():
-    data = torch.tensor([[1 for i in range(40)]], dtype=torch.int64)
-    graphs = dynamo_compiler.importer(model, data)
+    prefill_opt = torch.compile(prefill, backend=my_compiler)
+    prefill_opt(
+        model, prompt.view(1, -1), input_pos, temperature=1.0, top_k=None
+    )
+    # graphs = dynamo_compiler.importer(
+    #     prefill,
+    #     model,
+    #     prompt.view(1, -1),
+    #     input_pos,
+    #     temperature=1.0,
+    #     top_k=None,
+    # )
+    # print(len(graphs))
+    # graphs[0].lower_to_top_level_ir(do_params_pack=True)
+    # print(graphs[0]._imported_module)
 
-assert len(graphs) == 1
-graph = graphs[0]
-params = dynamo_compiler.imported_params[graph]
-graph.lower_to_top_level_ir(True)
-path_prefix = os.path.dirname(os.path.abspath(__file__))
-# Write the MLIR module to the file.
-with open(os.path.join(path_prefix, "llama.mlir"), "w") as module_file:
-    print(graph._imported_module, file=module_file)
+# # Import the model into MLIR module and parameters.
+# with torch.no_grad():
+#     data = torch.tensor([[1 for i in range(40)]], dtype=torch.int64)
+#     graphs = dynamo_compiler.importer(model, data)
 
-# Concatenate all parameters into a single numpy array and write to a file.
-all_param = numpy.concatenate(
-    [param.detach().numpy().reshape([-1]) for param in params]
-)
-all_param.tofile(os.path.join(path_prefix, "arg0.data"))
+# assert len(graphs) == 1
+# graph = graphs[0]
+# params = dynamo_compiler.imported_params[graph]
+# graph.lower_to_top_level_ir(True)
+# path_prefix = os.path.dirname(os.path.abspath(__file__))
+# # Write the MLIR module to the file.
+# with open(os.path.join(path_prefix, "llama.mlir"), "w") as module_file:
+#     print(graph._imported_module, file=module_file)
+
+# # Concatenate all parameters into a single numpy array and write to a file.
+# all_param = numpy.concatenate(
+#     [param.detach().numpy().reshape([-1]) for param in params]
+# )
+# all_param.tofile(os.path.join(path_prefix, "arg0.data"))
